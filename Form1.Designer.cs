6a33caf6026b9f33be724a3bbebbeb7fa711483f
卫尧 戚 <936804292@qq.com>
2019-06-17 22:07:25 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Sub = new System.Windows.Forms.Button();
+            this.btn_GetResult = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Mult = new System.Windows.Forms.Button();
+            this.btn_Transf = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.text_Row = new System.Windows.Forms.TextBox();
+            this.text_Colu = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 241);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(516, 182);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 133);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 133);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "矩阵A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "矩阵b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(771, 84);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(107, 64);
+            this.btn_Add.TabIndex = 6;
+            this.btn_Add.Text = "+";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
+            // 
+            // btn_Sub
+            // 
+            this.btn_Sub.Location = new System.Drawing.Point(771, 169);
+            this.btn_Sub.Name = "btn_Sub";
+            this.btn_Sub.Size = new System.Drawing.Size(107, 64);
+            this.btn_Sub.TabIndex = 11;
+            this.btn_Sub.Text = "-";
+            this.btn_Sub.UseVisualStyleBackColor = true;
+            this.btn_Sub.Click += new System.EventHandler(this.Btn_Sub_Click);
+            // 
+            // btn_GetResult
+            // 
+            this.btn_GetResult.Location = new System.Drawing.Point(68, 475);
+            this.btn_GetResult.Name = "btn_GetResult";
+            this.btn_GetResult.Size = new System.Drawing.Size(416, 73);
+            this.btn_GetResult.TabIndex = 15;
+            this.btn_GetResult.Text = "随机矩阵";
+            this.btn_GetResult.UseVisualStyleBackColor = true;
+            this.btn_GetResult.Click += new System.EventHandler(this.Btn_GetResult_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(38, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(628, 456);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "计算";
+            // 
+            // btn_Mult
+            // 
+            this.btn_Mult.Location = new System.Drawing.Point(771, 254);
+            this.btn_Mult.Name = "btn_Mult";
+            this.btn_Mult.Size = new System.Drawing.Size(107, 64);
+            this.btn_Mult.TabIndex = 17;
+            this.btn_Mult.Text = "*";
+            this.btn_Mult.UseVisualStyleBackColor = true;
+            this.btn_Mult.Click += new System.EventHandler(this.Btn_Mult_Click);
+            // 
+            // btn_Transf
+            // 
+            this.btn_Transf.Location = new System.Drawing.Point(561, 475);
+            this.btn_Transf.Name = "btn_Transf";
+            this.btn_Transf.Size = new System.Drawing.Size(105, 73);
+            this.btn_Transf.TabIndex = 18;
+            this.btn_Transf.Text = "转置";
+            this.btn_Transf.UseVisualStyleBackColor = true;
+            this.btn_Transf.Click += new System.EventHandler(this.Btn_Transf_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "矩阵A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "矩阵B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "结果";
+            // 
+            // text_Row
+            // 
+            this.text_Row.Location = new System.Drawing.Point(99, 76);
+            this.text_Row.Name = "text_Row";
+            this.text_Row.Size = new System.Drawing.Size(100, 21);
+            this.text_Row.TabIndex = 19;
+            // 
+            // text_Colu
+            // 
+            this.text_Colu.Location = new System.Drawing.Point(99, 130);
+            this.text_Colu.Name = "text_Colu";
+            this.text_Colu.Size = new System.Drawing.Size(100, 21);
+            this.text_Colu.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "设置矩阵行列";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(60, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "行";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(60, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 12);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "列";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.text_Row);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.text_Colu);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(704, 358);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(251, 190);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 614);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_Transf);
+            this.Controls.Add(this.btn_Mult);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_GetResult);
+            this.Controls.Add(this.btn_Sub);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Sub;
+        private System.Windows.Forms.Button btn_GetResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Mult;
+        private System.Windows.Forms.Button btn_Transf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_Row;
+        private System.Windows.Forms.TextBox text_Colu;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int[,] A = new int[4, 4];
+        int[,] B = new int[4, 4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_GetResult_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            Random rNum = new Random();
+            int n;//定义变量
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    n = rNum.Next(100);
+                    A[i, j] = n;//随机生成16个数值
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    textBox1.Text = textBox1.Text + A[i, j].ToString().PadRight(6);
+                    //将矩阵A的数据写入textBox1
+                }
+                textBox1.Text += Environment.NewLine;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    n = rNum.Next(16);
+                    B[i, j] = n;//随机生成16个数值
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    textBox2.Text = textBox2.Text + B[i, j].ToString().PadRight(6);
+                    //将矩阵A的数据写入textBox2
+                }
+                textBox2.Text += Environment.NewLine;
+            }
+
+
+        }
+
+        private void Btn_Mult_Click(object sender, EventArgs e)
+        {
+
+            int[,] C = new int[4, 4];
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    for (int k = 0; k < 4; k++) {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    textBox3.Text = textBox3.Text + C[i, j].ToString().PadRight(6);
+                    //将矩阵C中的数据写入textBox3
+                }
+                textBox3.Text += Environment.NewLine;
+            }
+
+        }
+
+
+        //矩阵加法
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+       
+                int[,] C = new int[4, 4];
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int m = 0; m < 4; m++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            C[m, j] = A[m, j] + B[m, j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        textBox3.Text = textBox3.Text + C[i, j].ToString().PadRight(6);
+                        //将矩阵C中的数据写入textBox3
+                    }
+                    textBox3.Text += Environment.NewLine;
+                }
+   
+        }
+       
+        //矩阵减法
+        private void Btn_Sub_Click(object sender, EventArgs e)
+        {
+
+                int[,] C = new int[4, 4];
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int m = 0; m < 4; m++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            C[m, j] = A[m, j] - B[m, j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        textBox3.Text = textBox3.Text + C[i, j].ToString().PadRight(6);
+                        //将矩阵C中的数据写入textBox3
+                    }
+                    textBox3.Text += Environment.NewLine;
+                }
+   
+        }
+
+        private void Btn_Transf_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            Random rNum = new Random();
+            int n;//定义变量
+            try
+            {
+                if (text_Row.Text.Length!=0|| text_Colu.Text.Length!=0)
+                {
+                    int _row = Convert.ToInt32(text_Row.Text);
+                    int _colu = Convert.ToInt32(text_Colu.Text);
+                    Matrix NMat = new Matrix(_row, _colu);//初始化矩阵行列
+                    
+                    for (int i = 0; i < _row; i++)
+                    {
+                        for (int j = 0; j < _colu; j++)
+                        {
+                            n= rNum.Next(100);
+                            NMat.data[i, j] = n;
+                        }
+                    }
+
+                    //将矩阵A的数据写入textBox1
+                    for (int i = 0; i < _row; i++)
+                    {
+                        for (int j = 0; j < _colu; j++)
+                        {
+                            textBox1.Text = textBox1.Text + NMat.data[i, j].ToString().PadRight(6);
+                        }
+                        textBox1.Text += Environment.NewLine;
+                    }
+
+                    //转置计算
+                    Matrix TransRes = Matrix.transpose(NMat);
+
+                    //将矩阵A转置后显示在textBox3结果中
+                    for (int i = 0; i < _colu; i++)
+                    {
+                        for (int j = 0; j < _row; j++)
+                        {
+                            textBox3.Text = textBox3.Text + TransRes.data[i, j].ToString().PadRight(6);
+                        }
+                        textBox3.Text += Environment.NewLine;
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("请输入行列！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
